665ec929d13d497cfefe3ee70fc7097c8aa58bc7
Igor Barbosa Ramalho <60898867+igor007-cyber@users.noreply.github.com>
2020-09-22 21:16:35 -0300

    aprendendo c++

---
+++ exercicio5.cpp
@@ -0,0 +1,18 @@
+/*
+5) Faça um Programa que converta metros para centímetros.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int x;
+
+  cout << "digite um numero em metros para converter em centimetros: " << endl;
+  cin >> x;
+
+  cout << x * 100 << " cm";
+
+  return 0;
+}
